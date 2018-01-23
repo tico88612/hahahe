f58ead33a6e08ec2e12d3287bb57c825a61c3984
Yang Cheng-Hao C9 <tico88612@gmail.com>
2018-01-23 10:04:50 +0000

    News Class Response

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'pages#news'
   get '/about' => 'pages#about'
-  get '/race' => 'pages#race'
+  get '/route' => 'pages#route'
   get '/score' => 'pages#score'
 end
